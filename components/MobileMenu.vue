2270bddbadf310a4c8d5b84107d5d9a4d06e377d
YSchilling <78358953+YSchilling@users.noreply.github.com>
2024-10-07 19:02:33 +0200

    made header and footer responsive

---
+++ components/MobileMenu.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+
+const menuActive = ref(false);
+const isDarkMode = useState("isDarkMode", () => false);
+
+</script>
+
+<template>
+    <!-- Icon -->
+    <svg class="ham hamRotate ham8 z-50 block md:hidden -mr-4" :class="menuActive ? 'active' : ''" viewBox="0 0 100 100"
+        width="56" @click="menuActive = !menuActive;">
+        <path class="line top" :stroke="isDarkMode || menuActive ? 'white' : 'black'"
+            d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
+        <path class="line middle" :stroke="isDarkMode || menuActive ? 'white' : 'black'" d="m 30,50 h 40" />
+        <path class="line bottom" :stroke="isDarkMode || menuActive ? 'white' : 'black'"
+            d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+    </svg>
+
+    <!-- Menu -->
+    <div class="mmcontainer duration-300 flex flex-col items-center z-40" :class="menuActive ? 'active' : ''">
+        <div class="flex items-center self-start text-white h-16">
+            <button @click="isDarkMode = false" :class="isDarkMode ? '' : 'font-extrabold'"
+                class="duration-300 hover:font-extrabold">Light</button>
+            <div class="text-xl mx-1">/</div>
+            <button @click="isDarkMode = true" :class="isDarkMode ? 'font-extrabold' : ''"
+                class="duration-300 hover:font-extrabold">Dark</button>
+        </div>
+        <ul class="w-full flex flex-col items-center mt-12 text-white">
+            <li @click="menuActive = false" class="text-2xl mb-6">
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li @click="menuActive = false" class="text-2xl mb-6">
+                <NuxtLink to="/about">About</NuxtLink>
+            </li>
+            <li @click="menuActive = false" class="text-2xl mb-6">
+                <NuxtLink to="/projects">Projects</NuxtLink>
+            </li>
+            <li @click="menuActive = false" class="text-2xl">
+                <NuxtLink to="/contact">Contact</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.mmcontainer {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    top: -100vh;
+    padding: 0 32px 0 32px;
+    background-color: rgba(0, 0, 0, 0.8)
+}
+
+.mmcontainer.active {
+    top: 0;
+}
+
+.ham {
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+    transition: transform 400ms;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+.hamRotate.active {
+    transform: rotate(45deg);
+}
+
+.line {
+    fill: none;
+    transition: stroke-dasharray 400ms, stroke-dashoffset 400ms;
+    stroke-width: 5.5;
+    stroke-linecap: round;
+}
+
+.ham8 .top {
+    stroke-dasharray: 40 160;
+}
+
+.ham8 .middle {
+    stroke-dasharray: 40 142;
+    transform-origin: 50%;
+    transition: transform 400ms;
+}
+
+.ham8 .bottom {
+    stroke-dasharray: 40 85;
+    transform-origin: 50%;
+    transition: transform 400ms, stroke-dashoffset 400ms;
+}
+
+.ham8.active .top {
+    stroke-dashoffset: -64px;
+}
+
+.ham8.active .middle {
+    transform: rotate(90deg);
+}
+
+.ham8.active .bottom {
+    stroke-dashoffset: -64px;
+}
+</style>
--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -5,21 +5,21 @@ const isDarkMode = useState("isDarkMode", () => false);
 </script>
 
 <template>
-    <footer class="grid grid-cols-3 items-center mx-auto max-w-screen-xl py-6 px-12">
-        <div>
-            <h1 class="text-2xl font-bold">Yorick Schilling</h1>
+    <footer class="grid grid-col-1 md:grid-cols-3 justify-items-center mx-auto max-w-screen-xl py-6 px-12">
+        <div class="flex flex-col row-start-3 md:row-start-1">
+            <h1 class="text-2xl font-bold self-center md:self-start">Yorick Schilling</h1>
             <p class="text-sm">© 2024 Yorick Schilling. All rights reserved.</p>
         </div>
-        <div class="flex flex-col items-center justify-self-center">
+        <div class="mb-8 md:mb-0 flex flex-col items-center justify-self-center row-start-2 md:row-start-1">
             <NuxtLink to="/about" class="duration-300 hover:font-extrabold">About</NuxtLink>
             <NuxtLink to="/projects" class="duration-300 hover:font-extrabold">Projects</NuxtLink>
             <NuxtLink to="/contact" class="duration-300 hover:font-extrabold">Contact</NuxtLink>
         </div>
-        <div class="justify-self-end flex gap-2">
-            <a href="https://www.linkedin.com/in/yschilling/" target="_blank">
+        <div class="mb-8 md:mb-0 md:justify-self-end self-center flex gap-2">
+            <a href="https://www.linkedin.com/in/yschilling/" target="_blank" class="hover:scale-110 duration-300">
                 <img src="/img/linkedin.png" width="32px" height="32px">
             </a>
-            <a href="https://github.com/YSchilling" target="_blank">
+            <a href="https://github.com/YSchilling" target="_blank" class="hover:scale-110 duration-300">
                 <img :src="isDarkMode ? '/vector/github-mark-white.svg' : '/vector/github-mark.svg'" width="32px"
                     height="32px">
             </a>
