f7b5c4335bf22d938003af4ff0036e9784b37d4b
YSchilling <78358953+YSchilling@users.noreply.github.com>
2024-10-24 23:51:31 +0200

    added impressum und datenschutzerklaerung

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -9,7 +9,12 @@ const isDarkMode = useState("isDarkMode", () => false);
         class="grid grid-col-1 md:grid-cols-3 items-center justify-items-center md:justify-items-start w-full mx-auto max-w-screen-xl py-6 px-6 md:px-8 lg:px-12">
         <div class="flex flex-col row-start-3 md:row-start-1">
             <h1 class="text-2xl font-bold self-center md:self-start">Yorick Schilling</h1>
-            <p class="text-sm">© 2024 Yorick Schilling. All rights reserved.</p>
+            <div>
+                <NuxtLink to="/impressum" class="duration-300 hover:font-extrabold">Impressum</NuxtLink>
+                -
+                <NuxtLink to="/datenschutzerklaerung" class="duration-300 hover:font-extrabold">Datenschutzerklärung
+                </NuxtLink>
+            </div>
         </div>
         <div class="mb-8 md:mb-0 flex flex-col items-center justify-self-center row-start-2 md:row-start-1">
             <NuxtLink to="/about" class="duration-300 hover:font-extrabold">About</NuxtLink>
---
+++ pages/impressum.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <MySection class="pt-32">
+            <h1 class="mb-8">Impressum</h1>
+            <p>Yorick Schilling</p>
+            <p>Kleine Gasse 20</p>
+            <p class="mb-4">61130 Nidderau</p>
+            <h2 class="text-2xl mb-1">Kontakt</h2>
+            <p class="mb-4">E-Mail: contact@yorickschilling.com</p>
+            <h2 class="text-2xl mb-1">Verbraucherstreitbeilegung/Universalschlichtungsstelle</h2>
+            <p class="mb-4">Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+                Verbraucherschlichtungsstelle teilzunehmen.</p>
+            <div>Quelle: <a class="underline hover:no-underline" href="https://www.e-recht24.de"
+                    target="_blank">https://www.e-recht24.de</a></div>
+
+        </MySection>
+    </main>
+</template>
+
+<style scoped>
+h1 {
+    font-size: clamp(32px, 6vw, 48px);
+}
+</style>
